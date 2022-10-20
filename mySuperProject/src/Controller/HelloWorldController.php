7008f53042cc0782635afab163a8dd6d5edf3784
raphaelbacco <raphael@bacco.fr>
2022-10-20 13:09:12 +0200

    add mySuperProject

---
+++ mySuperProject/src/Controller/HelloWorldController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController extends AbstractController
+{
+    #[Route('/hello', name: 'hello_world')]
+    public function helloWorld(Request $request)
+    {
+        //$response = new Response();
+        //$response->setContent("<h1>Hello World 2!</h1>");
+        //$response->setStatusCode(200);
+        //return $response;
+
+        $response = $this->render('helloworld/hello.html.twig');
+        return $response;
+    }
+
+    #[Route('/hello/{name}', name: 'hello_name')]
+    public function helloName(Request $request, string $name)
+    {
+        //$response = new Response();
+        //$response->setContent("<h1>Hello $name !</h1>");
+        //$response->setStatusCode(200);
+        //return $response;
+
+        $response = $this->render('helloworld/hello.html.twig', [
+            'name' => $name,
+        ]);
+        return $response;
+
+    }
+
+}
